3e54ec4d1547ce8e0cebfab72051bd957608c340
Wade Naughton <wadenaughton@gmail.com>
2021-10-19 18:55:29 -0400

    add ship lib and spec

---
+++ spec/ship_spec.rb
@@ -0,0 +1,26 @@
+require './lib/ship'
+
+RSpec.describe Ship do
+  it 'exists' do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser).to be_instance_of(Ship)
+  end
+
+  it 'has health' do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser.health).to eq(3)
+  end
+
+  it "is sunk?" do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser.sunk?).to eq("false")
+  end
+
+  it "takes a hit" do
+    cruiser = Ship.new("Cruiser", 3)
+    cruiser.hit
+    expect(cruiser.health).to eq(2)
+
+  end
+
+end
