72bd7f919bae9155f6433ed00f4302f2172d3af0
Russell Rockwood <russell.rockwood@gmail.com>
2021-10-19 17:46:57 -0500

    add initialize and tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,1 +1,29 @@
 require './lib/cell'
+
+
+describe Cell do
+  before(:each) do
+    @cell = Cell.new('B4')
+  end
+
+  describe '#initialize' do
+    it 'creates an instance of Cell' do
+      expect(@cell).to be_a(Cell)
+    end
+
+    it 'has a coordinate' do
+      expect(@cell.coordinate).to eq 'B4'
+    end
+  end
+
+  describe '#ship' do
+    it 'returns nil if no ship occupies the cell' do
+      expect(@cell.ship).to eq(nil)
+    end
+    # Alter to return a Ship object
+    it 'returns a ship object' do
+      expect(@cell.ship('Ship')).to eq('Ship')
+    end
+  end
+
+end
