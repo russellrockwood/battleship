bda520fddadd94555051ac5e9d2858c2cacfd63c
Wade Naughton <wadenaughton@gmail.com>
2021-10-21 22:41:20 -0400

    add initialize and sort_elements methods

--- lib/board_coords.rb
+++ lib/board_coords.rb
@@ -4,6 +4,22 @@ class BoardCoords
 
   def initialize(coordinates)
     @coordinates = coordinates
+
+  end
+
+  def sort_elements
+    split_coordinates = @coordinates.map do |coordinate|
+      coordinate.chars
+    end
+    letters = []
+    nums = []
+    split_coordinates.each do |array|
+      letters << array[0].ord
+      nums << array[1].to_i
+    end
+
+    nums = nums.sort
+    letters = letters.sort
   end
 
 end
