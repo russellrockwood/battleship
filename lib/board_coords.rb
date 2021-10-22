6c767459a9cc6d1fc102fc91c9271a40966f86a4
Wade Naughton <wadenaughton@gmail.com>
2021-10-21 22:58:01 -0400

    add valid_letters? method

--- lib/board_coords.rb
+++ lib/board_coords.rb
@@ -1,9 +1,11 @@
 class BoardCoords
 
-  attr_accessor :coordinates
+  attr_accessor :coordinates, :nums, :letters
 
   def initialize(coordinates)
     @coordinates = coordinates
+    @letters = []
+    @nums = []
 
   end
 
@@ -11,15 +13,29 @@ class BoardCoords
     split_coordinates = @coordinates.map do |coordinate|
       coordinate.chars
     end
-    letters = []
-    nums = []
+
     split_coordinates.each do |array|
-      letters << array[0].ord
-      nums << array[1].to_i
+      @letters << array[0].ord
+      @nums << array[1].to_i
     end
 
-    nums = nums.sort
-    letters = letters.sort
+    @nums = @nums.sort
+    @letters = @letters.sort
+  end
+
+  def valid_letters?
+    # letter_flag = true
+    con_array = letters.each_cons(2).to_a
+    con_array.each do |arr|
+      if arr[0] != arr[1] -1
+        false #flag = false previously
+      end
+    end
+    true
+  end
+
+  def valid_nums?
+
   end
 
 end
